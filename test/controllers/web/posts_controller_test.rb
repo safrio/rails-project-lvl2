ea2b866beb67e922563f4e1901869d055b30beb3
Ivan Safronov <ivan.safronov@mtxc.eu>
2022-01-30 20:50:04 +0500

    update tests

---
+++ test/controllers/web/posts_controller_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class Web::PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @post = posts(:one)
+  end
+
+  test 'should get index' do
+    get root_url
+    assert_response :success
+  end
+
+  test 'should get redirect to login' do
+    get new_post_url
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'should get new' do
+    user = users(:one)
+    sign_in(user)
+
+    get new_post_url
+    assert_response :success
+  end
+
+  test 'should create post' do
+    user = users(:one)
+    sign_in(user)
+
+    assert_difference('Post.count') do
+      post posts_url, params: { post: @post.attributes }
+    end
+
+    assert_redirected_to root_url
+  end
+
+  test 'should show post' do
+    get post_url(@post)
+    assert_response :success
+  end
+end
