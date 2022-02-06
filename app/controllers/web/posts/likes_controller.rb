3ce3d47c4cee1a2d4a9f03b65130d3233b844931
Ivan Safronov <ivan.safronov@mtxc.eu>
2022-02-06 23:28:43 +0500

    fix like doubles

--- app/controllers/web/posts/likes_controller.rb
+++ app/controllers/web/posts/likes_controller.rb
@@ -3,19 +3,25 @@
 module Web
   module Posts
     class LikesController < ApplicationController
+      before_action :post_by_params
+
       def create
-        PostLike.create(like_params)
+        PostLike.create(like_params) unless post_by_params.present?
 
         redirect_to post_path(params[:post_id])
       end
 
       def destroy
-        PostLike.find_by(like_params).delete
+        post_by_params&.delete
 
         redirect_to post_path(params[:post_id])
       end
 
       private
+
+      def post_by_params
+        PostLike.find_by(like_params)
+      end
 
       def like_params
         { post_id: params[:post_id], user: current_user }
