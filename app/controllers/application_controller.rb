f43280d61cff6d1486bf791461e64cb677f633dd
Ivan Safronov <ivan.safronov@mtxc.eu>
2022-02-10 20:03:21 +0500

    fix en locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  before_action :switch_locale # why instead of around_action: https://github.com/heartcombo/devise/issues/5247#issuecomment-659655504
 
-  def switch_locale(&action)
-    locale = params[:locale] || cookies[:locale] || I18n.default_locale
-    cookies[:locale] = locale
-
-    I18n.with_locale(locale, &action)
+  def switch_locale
+    I18n.locale = cookies[:locale] = params[:locale] || cookies[:locale] || I18n.default_locale
   end
 end
